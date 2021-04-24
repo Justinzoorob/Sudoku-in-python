6bcecccee055976253a76547649d6d9ae1ede529
Justinzoorob <71092751+Justinzoorob@users.noreply.github.com>
2021-04-24 14:20:22 -0400

    Add files via upload

---
+++ Game.py
@@ -0,0 +1,132 @@
+﻿def verifierLigne(grille, row, num):
+    '''
+        (list, int, int) -> Bool
+        Check if the box to add does not exist on the line.
+        Preconditions: grille is a reference to a 9x9 matrix which already contains numbers from 1 to 9
+    '''
+    x = -1
+    for i in grille:
+        x += 1
+        if x == row:
+            if num not in i: 
+                z = True
+            else: 
+                z = False
+            break
+    return z
+
+def verifierCol(grille, col, num):
+    '''
+        (list, int, int) -> Bool
+        Check if the box to add does not exist on the column.
+        Preconditions: grille is a reference to a 9x9 matrix which already contains numbers from 1 to 9   
+    '''
+    x = -1
+    for i in grille:
+        x += 1
+        z = True
+        if grille[x][col] == num:
+            z = False
+            break
+    return z
+
+
+def verifierSousGrille(grille, row, col, num):
+    '''
+        (list, int, int, int) -> Bool
+        Check if the box to add does not exist on the sub-grid.
+        Preconditions: grille is a reference to a 9x9 matrix which already contains numbers from 1 to 9
+    '''
+    ls = []
+    x = False
+    if row < 3 and col < 3:
+        for i in range(0,3):
+            for k in range(0,3):
+                ls.append(grille[i][k])
+        if num not in ls:
+            x = True
+
+    elif  row < 3 and 3 <= col < 6:
+        for i in range(0,3):
+            for k in range(3,6):
+                ls.append(grille[i][k])
+        if num not in ls:
+            x = True
+
+    elif row < 3 and 6 <= col:
+        for i in range(0,3):
+            for k in range(6,9):
+                ls.append(grille[i][k])
+        if num not in ls:
+            x = True
+
+    elif 3 <= row < 6 and col < 3:
+        for i in range(3,6):
+            for k in range(0,3):
+                ls.append(grille[i][k])
+        if num not in ls:
+            x = True
+
+    elif 3 <= row < 6 and 3 <= col < 6:
+        for i in range(3,6):
+            for k in range(3,6):
+                ls.append(grille[i][k])
+        if num not in ls:
+            x = True
+
+    elif 3 <= row < 6 and 6 <= col:
+        for i in range(3,6):
+            for k in range(6,9):
+                ls.append(grille[i][k])
+        if num not in ls:
+            x = True
+
+    elif 6 <= row and col < 3:
+        for i in range(6,9):
+            for k in range(0,3):
+                ls.append(grille[i][k])
+        if num not in ls:
+            x = True
+
+    elif 6 <= row and 3 <= col < 6:
+        for i in range(6,9):
+            for k in range(3,6):
+                ls.append(grille[i][k])
+        if num not in ls:
+            x = True
+
+    elif 6 <= row and 6 <= col:
+        for i in range(6,9):
+            for k in range(6,9):
+                ls.append(grille[i][k])
+        if num not in ls:
+            x = True
+
+    return x
+
+    
+def verifierGagner(grille):
+    '''(list) ->  bool
+    * Preconditions: grille is a reference to a 9x9 matrix which already contains numbers from 1 to 9
+    * Checks if the grid is completely filled.
+    '''
+    x = True
+    for i in grille:
+        for k in i:
+            if k == 0:
+                x = False
+    return x 
+  
+def verifierValide(grille, row, col, num):
+   ''' (list, int, int, int) ->  bool
+   * Checks if the proposed number is correct on the row and column and the sub-grid given in parameter.
+   * Preconditions: grille is a reference to a 9x9 matrix which already contains numbers from 1 to 9
+   '''
+   if verifierLigne(grille, row, num) == True and verifierCol(grille, col, num) == True and verifierSousGrille(grille, row, col, num) == True:
+       x = True
+   else:
+        x = False
+   return x
+    
+   
+
---
+++ Sudoku.py
@@ -0,0 +1,89 @@
+﻿from Game import *
+
+def afficherGrille(grille):
+    '''
+    (list) -> None
+    Displays the Sudoku game grid
+    Preconditions: grille is a reference to a 9x9 matrix which already contains numbers from 1 to 9
+    '''
+    print("   ", end="")
+    col = 0
+    while col < len(grille):
+      print(col, end="  ")
+      col += 1
+    print()
+    row = 0
+    while row < len(grille):
+       print(row, end="")
+       col = 0
+       while col < len(grille[row]):
+         print(" ", grille[row][col], end="")
+         col += 1
+       print()
+       row += 1
+
+def jouer(grille, row, col, num):
+    '''
+    (list, int, int, int) -> Bool
+    Play a Sudoku game step
+    Preconditions: grille is a reference on a 9x9 list contains only numbers
+    grid is modified (an element is added in the grid) if the box is valid is correct.
+    '''
+    x = False
+    if verifierValide(grille, row, col, num) == True:
+        grille[row][col] = num
+        x = True
+    return x
+
+
+# Main Programme
+
+# Create the game board (9 x 9)
+grille = [[5, 3, 8, 6, 9, 1, 0, 4, 7],
+          [7, 4, 6, 0, 3, 2, 8, 1, 9],
+          [1, 9, 2, 0, 8, 4, 3, 5, 6],
+          [8, 7, 1, 2, 6, 3, 4, 9, 5],
+          [3, 2, 9, 4, 5, 7, 1, 6, 8],
+          [4, 6, 5, 9, 1, 8, 7, 2, 3],
+          [0, 0, 4, 3, 7, 9, 5, 8, 2],
+          [9, 8, 3, 1, 0, 5, 6, 7, 4],
+          [2, 5, 0, 8, 4, 6, 9, 3, 1]]  # the only matrix used in the program.
+print("Menu: 1- Start a new game.")
+print("     ", "2- Continue the game.")
+print("     ", "3- Quit the game.")
+choix = int(input("Please enter your choice: 1, 2 ou 3: "))
+while choix < 3 and choix > 0:
+      if choix == 1:
+          # Create the game board (9 x 9)
+          grille = [[5, 3, 8, 6, 9, 1, 0, 4, 7],
+                    [7, 4, 6, 5, 3, 2, 8, 1, 9],
+                    [1, 9, 2, 7, 8, 4, 3, 5, 6],
+                    [8, 7, 1, 2, 6, 3, 4, 9, 5],
+                    [3, 2, 9, 4, 5, 7, 1, 6, 8],
+                    [4, 6, 5, 9, 1, 8, 7, 2, 3],
+                    [6, 1, 4, 3, 7, 9, 5, 8, 2],
+                    [9, 8, 3, 1, 2, 5, 6, 7, 4],
+                    [2, 5, 0, 8, 4, 6, 9, 3, 1]]  # the only matrix used in the program.
+      afficherGrille(grille)
+      row = int(input("Please enter the row index: "))
+      col = int(input("Please enter the Column index: "))
+      num = int(input("Please enter the number you would like: "))
+      if grille[row][col] == 0:
+          ajout = jouer(grille, row, col, num)
+          if ajout == True:
+              print("Good Job!")
+          else:
+              print("Failure :(")
+      else:
+          print("Box already filled")
+      afficherGrille(grille)
+      if verifierGagner(grille) == True:
+          print("Good Job you have Won")
+          choix = -1
+      else:
+        print("Menu: 1- Start a new game.")
+        print("     ", "2- Continue the game.")
+        print("     ", "3- Quit the game.")
+        choix = int(input("Please enter your choice: 1, 2 ou 3: "))
+if choix==3:
+    print("Goodbye :)")
